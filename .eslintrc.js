dc8fe46a1abd4d58fe8317aa7a9131f515ad1295
Cory Perkins <link2cory@gmail.com>
2020-11-08 17:12:48 -0800

    removed root level eslint as it was causing problems

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["airbnb-typescript/base"],
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  rules: {
-    "@typescript-eslint/quotes": ["error", "double"],
-  },
-};
